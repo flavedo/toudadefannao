34744568b2b493ebdcd222e4284aa00c08f2e451
chengjunsen <chengjunsen@kingsoft.com>
2018-08-01 18:59:33 +0800

    完善列表

--- component/sheetitem/sheetitem.js
+++ component/sheetitem/sheetitem.js
@@ -2,22 +2,23 @@
 Component({
 
   properties: {
-    page: Number,
-    sheettype: Number,
+    subject: Object,
+    option: Object,
     num: Number
   },
 
   data: {
-
+    sheettype: 1
   },
 
   methods: {
     tapFile() {
+      let { subject, option } = this.properties
       wx.vibrateShort()
       let pages = getCurrentPages();
-      if (pages.length > 1) {
+      if (pages.length > 1 && subject) {
         let prePage = pages[pages.length - 2];
-        prePage.$data.page = this.properties.page
+        prePage.$data.page = this.properties.num
         prePage.loadPage()
         wx.navigateBack()
       }
